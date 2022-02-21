768b035da1c54e7cd8f59e35c65ba2a68d99b10d
yamikjg4 <yamikgandhi@outlook.com>
2022-02-21 15:15:08 +0530

    newchanges

---
+++ UserValidation/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserValidation.Models;
+
+namespace UserValidation.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IHttpContextAccessor _http;
+        public AccountController(IHttpContextAccessor http)
+        {
+            _http = http;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Employee emp) 
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.message = "Inserted Successfully";
+                _http.HttpContext.Session.SetString("username",emp.Ename);
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                return View(emp);
+            }
+        }
+    }
+}
---
+++ UserValidation/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserValidation.Models
+{
+    public class Employee
+    {
+        [Required]
+        [Display(Name ="Employee Id")]
+        public int id { get; set; }
+        [Display(Name ="Employee Name"),Required]
+        public string Ename { get; set; }
+        [Display(Name = "Email Address"), Required, EmailAddress]
+        public string Email { get; set; }
+        [DataType(DataType.PhoneNumber), Required]
+        [Display(Name = "Mobile No")]
+        public string phone { get; set; }
+    }
+}
